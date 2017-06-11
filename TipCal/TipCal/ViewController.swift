bbc82ab776dcb8ee5ad400f6de3884bf68f85613
ravi <ravistakumar@gmail.com>
2017-06-11 13:01:25 -0400

    UI Design

--- TipCal/TipCal/ViewController.swift
+++ TipCal/TipCal/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountBeforeTip: UITextField!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+    @IBOutlet weak var tipAmount: UITextField!
+    @IBOutlet weak var totalAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
